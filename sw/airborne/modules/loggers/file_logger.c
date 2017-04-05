0c66eb9071272f1fb9c0708f77c8fe728661995f
wv-tud <w.vlenterie@student.tudelft.nl>
2017-04-05 08:40:29 +0200

    File logger update

--- sw/airborne/modules/loggers/file_logger.c
+++ sw/airborne/modules/loggers/file_logger.c
@@ -64,7 +64,8 @@ void file_logger_start(void)
   if (file_logger != NULL) {
     fprintf(
       file_logger,
-      "counter,pos_NED_x, pos_NED_y, pos_NED_z, filt_accel_ned_x, filt_accel_ned_y, filt_accel_ned_z, quat_i, quat_x, quat_y, quat_z,  sp_quat_i, sp_quat_x, sp_quat_y, sp_quat_z, sp_accel_x, sp_accel_y, sp_accel_z, accel_ned_x, accel_ned_y, accel_ned_z, speed_ned_x, speed_ned_y, speed_ned_z, imu_accel_unscaled_x, imu_accel_unscaled_y, imu_accel_unscaled_z, body_rates_p, body_rates_q, body_rates_r, actuaros_pprz_0, actuaros_pprz_1, actuaros_pprz_2, actuaros_pprz_3\n"
+      "counter,RAW_mag_x, RAW_mag_y, RAW_mag_z, SCALED_mag_x, SCALED_mag_y, SCALED_mag_z, psi\n"
+      // INDI LOG: "counter,pos_NED_x, pos_NED_y, pos_NED_z, filt_accel_ned_x, filt_accel_ned_y, filt_accel_ned_z, quat_i, quat_x, quat_y, quat_z,  sp_quat_i, sp_quat_x, sp_quat_y, sp_quat_z, sp_accel_x, sp_accel_y, sp_accel_z, accel_ned_x, accel_ned_y, accel_ned_z, speed_ned_x, speed_ned_y, speed_ned_z, imu_accel_unscaled_x, imu_accel_unscaled_y, imu_accel_unscaled_z, body_rates_p, body_rates_q, body_rates_r, actuaros_pprz_0, actuaros_pprz_1, actuaros_pprz_2, actuaros_pprz_3\n"
     );
   }
 }
@@ -85,12 +86,22 @@ void file_logger_periodic(void)
     return;
   }
   static uint32_t counter = 0;
+  struct FloatEulers* eulers = stateGetNedToBodyEulers_f();
+  fprintf(file_logger, "%d,%d,%d,%d,%f,%f,%f,%f\n",
+            counter,
+            imu.mag_unscaled.x,
+            imu.mag_unscaled.y,
+            imu.mag_unscaled.z,
+            MAG_FLOAT_OF_BFP(imu.mag.x),
+            MAG_FLOAT_OF_BFP(imu.mag.y),
+            MAG_FLOAT_OF_BFP(imu.mag.z),
+            eulers->psi);
+/*  INDI LOG
   struct Int32Quat * quat       = stateGetNedToBodyQuat_i();
   struct NedCoor_f * pos        = stateGetPositionNed_f();
   struct NedCoor_f * accel_ned  = stateGetAccelNed_f();
   struct NedCoor_f * speed_ned  = stateGetSpeedNed_f();
   struct FloatRates* rates_body = stateGetBodyRates_f();
-
   fprintf(file_logger, "%d,%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d,%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%f,%f,%f,%d,%d,%d,%d\n",
           counter,
           pos->x,
@@ -133,5 +144,6 @@ void file_logger_periodic(void)
           actuators_pprz[2],
           actuators_pprz[3]
          );
+*/
   counter++;
 }
